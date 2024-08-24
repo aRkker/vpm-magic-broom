ed8595b887d930d259cbceaef50ae934e191e000
aRkker <arkkerx@gmail.com>
2024-08-24 05:02:01 +0300

    0.0.1 initial

---
+++ Packages/com.arkker.vpm.magic-broom/Runtime/Scripts/PickupRespawnerBroom.cs
@@ -0,0 +1,30 @@
+﻿
+using MMMaellon;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class PickupRespawnerBroom : UdonSharpBehaviour
+{
+
+    VRCPickup pickup;
+
+    void Start()
+    {
+        pickup = GetComponent<VRCPickup>();
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Collision!");
+        if (other.gameObject.GetComponent<SmartObjectSync>() != null && pickup.IsHeld && Networking.IsOwner(pickup.gameObject))
+        {
+            // Debug.Log("Respawning!");
+            Networking.SetOwner(Networking.LocalPlayer, other.gameObject);
+            other.gameObject.GetComponent<SmartObjectSync>().Respawn();
+        }
+    }
+}
